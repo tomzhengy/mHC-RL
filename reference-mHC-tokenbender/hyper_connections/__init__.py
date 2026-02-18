d749c29b480ea270f0ea759f924051fc5e4c31af
tomzhengy <tomzheng1012@gmail.com>
2026-02-17 20:06:37 -0800

    chore: replace reference-mHC-tokenbender with git submodule

--- reference-mHC-tokenbender/hyper_connections/__init__.py
+++
@@ -1,8 +0,0 @@
-from hyper_connections.hyper_connections import (
-    HyperConnections,
-    get_expand_reduce_stream_functions,
-    get_init_and_expand_reduce_stream_functions,
-    Residual,
-    StreamEmbed,
-    AttentionPoolReduceStream
-)
